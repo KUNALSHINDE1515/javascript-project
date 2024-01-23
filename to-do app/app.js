160ec9594ad6f0a68483175e2683fda90acf6085
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-01-23 14:06:12 +0530

    ToDo

--- to-do app/app.js
+++ to-do app/app.js
@@ -2,10 +2,12 @@ const item = document.querySelector('#item');
 const toDoBox = document.querySelector('#to-do-box');
 
 item.addEventListener("keyup", function(event) {
-    if(event.key == "Enter");
-    addToDO(this.value);
-    this.value = ""
-})
+    if (event.key == "Enter") {
+        addToDO(this.value);
+        this.value = ""
+    }
+}, false)
+    
 
 const addToDO = (item) => {
     const listItem = document.createElement("li");
@@ -13,5 +15,12 @@ const addToDO = (item) => {
     ${item}
     <i class="fas fa-times"></i>`;
 
+    listItem.addEventListener('click', function(){
+        this.classList.toggle("done")
+    },false)
+
+    listItem.querySelector("i").addEventListener('click', function(){
+        listItem.remove()
+    },false)
     toDoBox.appendChild(listItem)
 }
