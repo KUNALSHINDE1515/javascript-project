b758751755a042fed769fa302a93db62bf2eeb4d
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-02-26 20:12:43 +0530

    functions practice

---
+++ 09_api/handel.js
@@ -0,0 +1,6 @@
+function api() {
+    const respose = "https://api.github.com/users/kunalshinde1515";
+
+    const data = respose.json();
+    console.log(data);
+}
---
+++ javascript Function Quetions Practice/functon.js
@@ -0,0 +1,48 @@
+// Write a JavaScript function to find the maximum of two numbers.
+function maximum(a , b) {
+    if (a < b) {
+        console.log("MAximum number is B");
+    }else{
+        console.log("MAximum number is A");
+    }
+
+}
+
+// maximum( 9 , 8)
+
+// Write a JavaScript function to find the factorial of a number.
+
+
+let number = 5
+function factorial(number) {
+
+    let fact = 1;
+
+    if (number === 0) 
+        return 1;
+    for (let i = 2; i <= number; i++) 
+        fact = fact  * i;
+        return fact;
+    
+}
+
+console.log(factorial(number));
+
+// Write a JavaScript function to reverse a string.
+// Write a JavaScript function to check if a number is prime.
+// Write a JavaScript function to check if a string is palindrome.
+// Write a JavaScript function to generate a random integer within a specified range.
+// Write a JavaScript function to calculate the area of a circle given its radius.
+// Write a JavaScript function to concatenate two arrays.
+// Write a JavaScript function to remove duplicate elements from an array.
+// Write a JavaScript function to convert Celsius to Fahrenheit.
+// Write a JavaScript function to find the longest word in a string.
+// Write a JavaScript function to count the number of vowels in a string.
+// Write a JavaScript function to sort an array of numbers in ascending order.
+// Write a JavaScript function to find the sum of digits of a positive integer.
+// Write a JavaScript function to capitalize the first letter of each word in a string.
+// Write a JavaScript function to check if an array contains a specific element.
+// Write a JavaScript function to generate a Fibonacci sequence of a specified length.
+// Write a JavaScript function to find the median of an array of numbers.
+// Write a JavaScript function to calculate the factorial of each element in an array.
+// Write a JavaScript function to find the intersection of two arrays.
